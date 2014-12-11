b9e5a2cb5b7ba5e694c4886941ce55ad20c60b13
r351574nc3 <r351574nc3@gmail.com>
2014-12-11 03:49:41 -0700

    Adding logo and background

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,41 @@
-var earshotHq = angular.module('earshotHq', ['ngAnimate']);
+angular.module('earshotHq', ['ngAnimate'])
+    .controller('HomeCtrl', ['$scope', function($scope) {
+        $scope.format = 'M/d/yy h:mm:ss a';
+    }]) 
+    .directive('rotatingHeader', ['$animate', '$interval', '$compile',
+                             function ($animate, $interval, $compile) {
+                                 return {
+                                     restrict: 'E',
+                                     link: function(scope, element, attrs) {
+                                         var timeoutId;
+                                         var text = element.text()
+                                         element.text('')
 
-earshotHq.controller('HomeCtrl', function ($scope) {
-});
+                                         scope.rotHeadIdx = 0;
+                                         
+                                         var fonts = eval(attrs.fonts);
+
+                                         function rotateHeader() {
+                                             angular.forEach(element.children(), function(toRem) {
+                                                 $animate.leave(toRem)
+                                                 toRem.remove()
+                                             })
+                                             
+                                             var font = fonts[scope.rotHeadIdx++];
+                                             
+                                             $animate.enter(angular.element('<h1 id="' + font + '">' + text + '</h1>'), element)
+                                         }
+
+                                         element.on('$destroy', function() {
+                                             $interval.cancel(timeoutId);
+                                         });
+
+                                         
+                                         rotateHeader();
+                                         timeoutId = $interval(function() {
+                                             rotateHeader();
+                                         }, 5000);
+                                         
+                                     }
+                                 };
+                             }])
